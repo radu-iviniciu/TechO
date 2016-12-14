eb7d1dc71eb632cb66090e3d445d794154e6cf7f
roivinir <radu.iviniciu@gmail.com>
2016-12-14 13:19:32 +0200

    Refactor

--- AmazonGenerator/AmazonGenerator/AmazonGenerator/Program.cs
+++ AmazonGenerator/AmazonGenerator/AmazonGenerator/Program.cs
@@ -15,7 +15,7 @@ namespace AmazonGenerator
         readonly string ObjectCount = "input_objects_count";
         readonly string Objects = "input_objects";
 
-        static readonly string testPath = "D:\\Amazon Techo(n)\\01_helloWorld_automatic.txt";
+        static readonly string testPath = "01_helloWorld_automatic.txt";
 
         static void Main(string[] args)
         {
@@ -26,14 +26,14 @@ namespace AmazonGenerator
 
     class PrgGenerator
     {
-        string _path;
-        StreamWriter _file;
 
+        // Predefined
         readonly string Rows = "input_rows";
         readonly string Cols = "input_cols";
         readonly string ObjectCount = "input_objects_count";
-        readonly string Objects = "input_objects";
+        readonly string Map = "input_objects";
 
+        // Need declaration
         readonly string wallCode = "1";
         readonly string targetCode = "2";
         readonly string destinationCode = "3";
@@ -55,274 +55,86 @@ namespace AmazonGenerator
         readonly string i = "i";
         readonly string j = "j";
 
+        private Writer _codeGen;
+
         public PrgGenerator(string path)
         {
-            _path = path;
-            _file = new StreamWriter(path);
+            _codeGen = new Writer(new StreamWriter(path));
         }
 
         public void GenerateProgram()
         {
-            Declare(robotX);
-            Declare(robotY);
-            Declare(count);
-            Declare(i);
-            Declare(j);
+            _codeGen.Declare(robotX, true);
+            _codeGen.Declare(robotY, true);
+            _codeGen.Declare(count, true);
+            _codeGen.Declare(i, true);
+            _codeGen.Declare(j, true);
 
-            Declare(curTargetX);
-            Declare(curTargetY);
+            _codeGen.Declare(curTargetX, true);
+            _codeGen.Declare(curTargetY, true);
 
-            DeclareArray(targets, 100);
+            _codeGen.DeclareArray(targets, 100, true);
 
-            Set(curTargetX, "0 1 -", true);
-            Set(curTargetY, "0 1 -", true);
+            _codeGen.Set(curTargetX, "0 1 -", true);
+            _codeGen.Set(curTargetY, "0 1 -", true);
 
             FindRobot();
 
-            _file.WriteLine("Find Targets");
+            _codeGen.Comment("Find Targets", true);
             FindPointsOfInterest(targets, targetCode, count);
-            _file.WriteLine("");
 
-            _file.WriteLine("Find Destinations");
+            _codeGen.Comment("Find Destinations", true);
             FindPointsOfInterest(destinations, destinationCode, count);
-            _file.WriteLine("");
 
-            _file.Flush();
-            _file.Close();
-        }
-
-        void Assert(string expr, string y)
-        {
-            AreEqual(y, expr, true);
-            _file.WriteLine("if");
-            _file.WriteLine("2");
-            _file.WriteLine("else");
-            _file.WriteLine("4");
-            _file.WriteLine("then");
+            _codeGen.Close();
         }
 
         int indentation = 0;
 
         void FindPointsOfInterest(string resultArray, string code, string localCounter)
         {
-            Set(i, 0, true);
-            Set(j, 0, true);
-            Set(localCounter, 0, true);
+            _codeGen.Set(i, 0, true);
+            _codeGen.Set(j, 0, true);
+            _codeGen.Set(localCounter, 0, true);
 
-            var curValue = GetArrayValue(Objects, Get(i));
-            var isObjectOfRightCode = AreEqual(curValue, code);
-            var isNotEndOfArray = Get(ObjectCount) + " 3 * " + " " + Get(i) + " > ";
+            var curValue = _codeGen.GetArrayValue(Map, _codeGen.Get(i));
+            var isObjectOfRightCode = _codeGen.AreEqual(curValue, code);
+            var isNotEndOfArray = _codeGen.Get(ObjectCount) + " 3 * " + " " + _codeGen.Get(i) + " > ";
 
-            While(isNotEndOfArray);
+            _codeGen.While(isNotEndOfArray, true);
             {
-                If(isObjectOfRightCode);
+                _codeGen.If(isObjectOfRightCode, true);
                 {
-                    SetArrraValue(targets, Get(j), GetArrayValue(Objects, Get(i) + " 1 + "), true);
-                    SetArrraValue(targets, Get(j) + " 1 + ", GetArrayValue(Objects, Get(i) + " 2 + "), true);
+                    _codeGen.SetArrraValue(resultArray, _codeGen.Get(j), _codeGen.GetArrayValue(Map, _codeGen.Get(i) + " 1 + "), true);
+                    _codeGen.SetArrraValue(resultArray, _codeGen.Get(j) + " 1 + ", _codeGen.GetArrayValue(Map, _codeGen.Get(i) + " 2 + "), true);
 
-                    Increment(j, 2, true);
-                    Increment(localCounter, true);
+                    _codeGen.Increment(j, 2, true);
+                    _codeGen.Increment(localCounter, true);
                 }
-                EndIf();
-                Increment(i, 3, true);
+                _codeGen.EndIf(true);
+                _codeGen.Increment(i, 3, true);
             }
-            Repeat(isNotEndOfArray);
+            _codeGen.Repeat(isNotEndOfArray, true);
 
-            Set(i, 0, true);
-            Set(j, 0, true);
-        }
-
-        private void EndIf()
-        {
-            _file.WriteLine("then");
-        }
-
-        void If(string condition)
-        {
-            _file.Write(condition + " ");
-            _file.WriteLine("if");
-        }
-
-        string Not(string x, bool writeToFile = false)
-        {
-            var not = x + " 1 <";
-            if (writeToFile)
-            {
-                _file.WriteLine(not);
-            }
-            return not;
-        }
-
-        string And(string x, string y, bool writeToFile = false)
-        {
-            var and = x + " = 1 " + y + " 1 =" + " 1 = ";
-            if (writeToFile)
-            {
-                _file.WriteLine(and);
-            }
-            return and;
+            _codeGen.Set(i, 0, true);
+            _codeGen.Set(j, 0, true);
         }
 
         void FindRobot()
         {
-            Set(robotX, "0 1 -", true);
-            Set(robotY, "0 1 -", true);
+            _codeGen.Set(robotX, "0 1 -", true);
+            _codeGen.Set(robotY, "0 1 -", true);
 
-            var isCurentObjectTheRobot = AreNotEqual(GetArrayValue(Objects, Get(i)), robotCode.ToString());
-            While(isCurentObjectTheRobot);
+            var isCurentObjectTheRobot = _codeGen.AreNotEqual(_codeGen.GetArrayValue(Map, _codeGen.Get(i)), robotCode.ToString());
+            _codeGen.While(isCurentObjectTheRobot, true);
             {
-                Increment(i, 3, true);
-                Repeat(isCurentObjectTheRobot);
+                _codeGen.Increment(i, 3, true);
+                _codeGen.Repeat(isCurentObjectTheRobot, true);
             }
 
-            Set(robotX, GetArrayValue(Objects, Get(i) + " 1 +"), true);
-            Set(robotY, GetArrayValue(Objects, Get(i) + " 2 +"), true);
-        }
-
-        // a condininal expresion on the stack
-        void While(string condition)
-        {
-            _file.WriteLine(condition);
-            _file.WriteLine("while");
-            indentation++;
-        }
-
-        void Repeat(string condition)
-        {
-            _file.WriteLine(condition);
-            _file.WriteLine("repeat");
-            indentation--;
-        }
-
-        string GreaterEqual(string x, string y, bool writeToFile = false) // 
-        {
-            var areEqual = x + " " + y + " >= ";
-            if (writeToFile)
-            {
-                _file.WriteLine(areEqual);
-            }
-            return areEqual;
-        }
-
-        // 1 or 0 to Stack
-        string AreNotEqual(string x, string y, bool writeToFile = false) // 
-        {
-            var areNotEqual = AreEqual(x, y) + " 1 < ";
-
-            if (writeToFile)
-            {
-                _file.WriteLine(areNotEqual);
-            }
-            return areNotEqual;
-        }
-
-        string AreEqual(string x, string y, bool writeToFile = false) // 
-        {
-            var areEqual = x + " " + y + " = ";
-            if (writeToFile)
-            {
-                _file.WriteLine(areEqual);
-            }
-            return areEqual;
-        }
-
-        string Declare(string variable)
-        {
-            var code = "variable " + variable;
-            _file.WriteLine(code);
-            return code;
-        }
-
-        string DeclareArray(string variable, int size)
-        {
-            var code = "variable " + variable + " " + size + " cells";
-            _file.WriteLine(code);
-            return code;
-        }
-
-        // Nothing on stack
-        string Increment(string x, bool writeToFile = false)
-        {
-            var code = x + " @ 1 + " + x + " ! ";
-            if (writeToFile)
-            {
-                _file.WriteLine(code);
-            }
-            return code;
-        }
-
-        // Nothing on stack
-        string Increment(string x, int value, bool writeToFile = false)
-        {
-            var code = x + " @ " + value + " + " + x + " ! ";
-            if (writeToFile)
-            {
-                _file.WriteLine(code);
-            }
-            return code;
-        }
-
-        string Increment(string x, string expresion, bool writeToFile = false)
-        {
-            var code = x + " @ " + expresion + " + " + x + " ! ";
-            if (writeToFile)
-            {
-                _file.WriteLine(code);
-            }
-            return code;
-        }
-
-        // Returns to stack
-        string GetArrayValue(string arrayAddr, string index, bool writeToFile = false)
-        {
-            var code = arrayAddr + " " + index + " + @ ";
-            if (writeToFile)
-            {
-                _file.WriteLine(code);
-            }
-            return code;
-        }
-
-        // no prereq on stack
-        string SetArrraValue(string arrayAddr, string index, string expresion, bool writeToFile = false)
-        {
-            var code = expresion + " " + arrayAddr + " " + index + " + ! ";
-            if (writeToFile)
-            {
-                _file.WriteLine(code);
-            }
-            return code;
-        }
-
-        // nothing on stack needed nor left
-        string Set(string x, string expr, bool writeToFile = false)
-        {
-            var code = expr + " " + x + " ! ";
-            _file.WriteLine(code);
-            return code;
-        }
-
-        // nothing on stack needed nor left
-        string Set(string x, int val, bool writeToFile = false)
-        {
-            var code = val + " " + x + " ! ";
-            if (writeToFile)
-            {
-                _file.WriteLine(code);
-            }
-            return code;
-        }
-
-        // Adds to stack
-        string Get(string x, bool writeToFile = false)
-        {
-            var code = x + " @ ";
-            if (writeToFile)
-            {
-                _file.WriteLine(code);
-            }
-            return code;
-        }
+            _codeGen.Set(robotX, _codeGen.GetArrayValue(Map, _codeGen.Get(i) + " 1 +"), true);
+            _codeGen.Set(robotY, _codeGen.GetArrayValue(Map, _codeGen.Get(i) + " 2 +"), true);
+        }      
     }
 }
 
